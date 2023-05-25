4dd5df96dad476c58fc553dc123b99d13b7edbe3
juanPablo96 <juan.pablo1996@hotmail.com>
2023-05-25 08:27:33 -0300

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,17 @@
+/* import { useState } from "react"; */
+
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Sticker from "./Components/Sticker/Sticker";
+
+function App() {
+  /*   const [count, setCount] = useState(0); */
+
+  return (
+    <>
+      <Sticker />
+    </>
+  );
+}
+
+export default App;
