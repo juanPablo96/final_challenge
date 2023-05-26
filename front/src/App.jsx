0eb03357fe2b98b2d6815f056b329ed729bff7b5
juanPablo96 <juan.pablo1996@hotmail.com>
2023-05-26 09:15:41 -0300

    creation of sticker component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   /*   const [count, setCount] = useState(0); */
 
   return (
-    <>
+    <div className="divAppContainer">
       <Main />
       <Aside />
-    </>
+    </div>
   );
 }
 
--- front/src/Components/Aside/Aside.jsx
+++ front/src/Components/Aside/Aside.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+/* import iniesta from "../../assets/iniesta.png"; */
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const Aside = () => {
+function Aside() {
   return (
-    <>
-      <div>Aside</div>
-    </>
+    <Card style={{ width: "12rem", marginLeft: "50px", height: "25rem" }}>
+      <Card.Img variant="top" /* src={iniesta} */ />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>España</Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
-};
+}
 
 export default Aside;
